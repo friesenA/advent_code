876f5c0b2b98407951a644c351c806b0701b9629
friesenA <friesenA@users.noreply.github.com>
2021-12-02 10:26:47 -0500

    sonar sweep script

---
+++ src/day_1/sonar_sweep.py
@@ -0,0 +1,15 @@
+"""Sonar Sweep"
+import DATA_POINTS from data
+
+
+if __name__ == '__main__':
+  count = 0
+
+  for i, value in enumerate(DATA_POINTS):
+    if i = 0:
+      continue
+    if value > dat_points[i-1]:
+      count += 1
+  
+  print(count)
+  
